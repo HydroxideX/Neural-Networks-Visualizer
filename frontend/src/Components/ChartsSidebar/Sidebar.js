1cd3a738e2bd71e3a8085998fe2ce3302c4d7d33
sh3ra <youssefalshaarawi@yahoo.com>
2020-09-11 14:00:50 +0200

    side bar

--- frontend/src/Components/ChartsSidebar/Sidebar.js
+++ frontend/src/Components/ChartsSidebar/Sidebar.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import ResizePanel from "react-resize-panel";
+import classNames from 'classnames/bind';
+import style from './style.css';
+let cx = classNames.bind(style);
 
 class Sidebar extends React.Component {
 
-  constructor (props) {
+  constructor(props) {
     super(props);
   }
 
   render() {
     return (
-      <div >
-      </div>
-    );
+      <div className={cx('container')}>
+        <div className={cx('body')}>
+          <ResizePanel direction="e" style={{ flexGrow: '1' }} >
+            <div className={cx('sidebar', 'withMargin', 'panel')}>left panel<br /> with margin <br />default 50% of content area using flex-grow</div>
+          </ResizePanel>
+          <div className={cx('content', 'panel')}>content</div>
+        </div>
+      </div>);
   }
 }
 
