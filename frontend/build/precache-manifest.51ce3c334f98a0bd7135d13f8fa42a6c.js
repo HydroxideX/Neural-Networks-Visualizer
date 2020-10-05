d9fee5f62b3dbfdb4675cca16c5c220ec457e746
HydroxideX <Yahiaelsaadawy@gmail.com>
2020-10-05 04:14:08 +0200

    created build

---
+++ frontend/build/precache-manifest.51ce3c334f98a0bd7135d13f8fa42a6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "058fd8235e38625db1e7609c960a1e1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97ee8a7515e647483a34",
+    "url": "/static/css/main.7329d716.chunk.css"
+  },
+  {
+    "revision": "dcbffd7fc3502d113266",
+    "url": "/static/js/2.a503af52.chunk.js"
+  },
+  {
+    "revision": "5e85a53f1813f713cf01b5e3fcff54f6",
+    "url": "/static/js/2.a503af52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97ee8a7515e647483a34",
+    "url": "/static/js/main.d81c2492.chunk.js"
+  },
+  {
+    "revision": "940f4f9afa97208f7c5d",
+    "url": "/static/js/runtime-main.a9591f21.js"
+  },
+  {
+    "revision": "0f45c05961dbe06e766c63494a19c11e",
+    "url": "/static/media/conv.0f45c059.png"
+  }
+]);
